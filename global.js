642befccb52d77b74bc8523fd8949234c6942d3b
xiayx <xiayouxue@hotmail.com>
2019-01-07 13:15:01 +0800

    全局变量和模块变量

---
+++ global.js
@@ -0,0 +1,6 @@
+console.info(`----全局变量----\n`);
+
+console.info(`global: `, global);
+console.info(`global.process: `, global.process);
+console.info(`global.process.mainModule: `, global.process.mainModule);
+console.info(`global.process.mainModule.exports: `, global.process.mainModule.exports);
---
+++ module.js
@@ -0,0 +1,7 @@
+console.info(`----局部变量----\n`);
+
+console.info(`__dirname: `, __dirname);
+console.info(`__filename: `, __filename);
+console.info(`exports: `, exports);
+console.info(`module: `, module);
+console.info(`require(): `, require);
